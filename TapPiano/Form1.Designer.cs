01d3acdf5fd583f3a7fe5e9a17ba586619270933
Eldad Kronfeld <eldadkro@gmail.com>
2023-07-31 01:11:56 +0300

    started GUI and WaveBuilder

---
+++ TapPiano/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TapPiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sound1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sound1
+            // 
+            this.sound1.Location = new System.Drawing.Point(13, 13);
+            this.sound1.Name = "sound1";
+            this.sound1.Size = new System.Drawing.Size(75, 23);
+            this.sound1.TabIndex = 0;
+            this.sound1.Text = "make Sound";
+            this.sound1.UseVisualStyleBackColor = true;
+            this.sound1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(340, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 255);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sound1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sound1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TapPiano/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TAPWin;
+using System.Media;
+
+namespace TapPiano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.log("start sound");
+            SoundPlayer player = new SoundPlayer(@"E:\Open-source\TapPiano\TapPiano\sounds/C_s.wav");
+            player.Play();
+
+        }
+
+        private void log(string line)
+        {
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                textBox1.AppendText(line + Environment.NewLine);
+            });
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
